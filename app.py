fcc2722ea5c1d79ca5971add5467dcb32fd54027
S0K123 <sonica.kesavan@gmail.com>
2026-04-08 22:37:38 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import subprocess
-
-app = FastAPI()
-
-@app.get("/")
-def run():
-    result = subprocess.check_output(["python", "inference.py"]).decode()
-    return {"output": result}
